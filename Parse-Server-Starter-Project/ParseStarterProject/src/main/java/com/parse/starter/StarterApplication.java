6cc80bff71c8eb81c63aa210386d8839c5d56cc4
nadaalsamahy <nadaalsamahy1810@gmail.com>
2019-03-04 22:38:17 +0200

    clash free time table

---
+++ Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId( "f33bc0e528cafbfdbc9931e98ed223e9fbcca7e4")
+            .clientKey("43b8a0fe8bb6b46449e1827c4ee433d45a8c4231")
+            .server("http://52.14.191.204:80/parse/")
+            .build()
+    );
+
+
+
+   // ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
